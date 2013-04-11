71a5b6d84ceaa0f83f9862c41fdf08773fe51117
jonathanklingberg <jonathan.klingberg@gmail.com>
2013-04-11 14:06:03 +0200

    Added "test" consol comment
    System.out.println

--- src/se/eda095/plumpkortspel/MainActivity.java
+++ src/se/eda095/plumpkortspel/MainActivity.java
@@ -18,5 +18,9 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+	
+	public static void main (String args[]){
+		System.out.println("test");
+	}
 
 }
