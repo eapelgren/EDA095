296d4a6e83520c602af30ee2a8280274ca85d675
Emil Apelgren <emilapelgren@hotmail.com>
2013-04-11 15:01:02 +0200

    EMil Version
    emil

--- src/se/eda095/plumpkortspel/MainActivity.java
+++ src/se/eda095/plumpkortspel/MainActivity.java
@@ -18,5 +18,21 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+<<<<<<< HEAD
 **////***
+=======
+	
+	public static void main (String args[]){
+		System.out.println("test2hej");
+	}
+
+	public void test() {
+		
+	}
+
+	
+	public static int getNbr(String a){
+		return 2;
+	}
+>>>>>>> 8873b94e5b93a9ae6b60da12e37d4105d781edf7
 }
